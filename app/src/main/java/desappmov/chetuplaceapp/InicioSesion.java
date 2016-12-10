c16966ef35856e37c7cce61c76b4d32d03dafcbe
yaret <JimenezC_Yaret@outlook.com>
2016-12-10 12:38:00 -0500

    Cardview

--- app/src/main/java/desappmov/chetuplaceapp/InicioSesion.java
+++ app/src/main/java/desappmov/chetuplaceapp/InicioSesion.java
@@ -1,8 +1,10 @@
 package desappmov.chetuplaceapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class InicioSesion extends Activity{
 
@@ -10,5 +12,16 @@ public class InicioSesion extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
+
+        //pasa al Siguiente layaut Inicio
+        findViewById(R.id.btnAceptar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(InicioSesion.this, Menu.class));
+            }
+        });
+
     }
+
+
 }
