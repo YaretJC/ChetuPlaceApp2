07228ea31c5342867bcddf0e682a474286b2b220
yaret <JimenezC_Yaret@outlook.com>
2016-12-10 18:36:09 -0500

    cerrar Sesion de usuario

--- app/src/main/java/desappmov/chetuplaceapp/Menu.java
+++ app/src/main/java/desappmov/chetuplaceapp/Menu.java
@@ -24,6 +24,55 @@ public class Menu extends AppCompatActivity {
             }
         });
     }
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuopciones, menu);
+        return true;
+    }*/
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        getMenuInflater().inflate(R.menu.menuopciones, menu);
+        return true;
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.opcion1) {
+            Toast.makeText(this, "Se seleccionó la primer opción", Toast.LENGTH_LONG).show();
+        }
+        if (id == R.id.opcion2) {
+            AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+            dialogo1.setTitle("Cerrar Sesion");
+            dialogo1.setMessage("¿Desea salir de ChetuPlace?");
+            dialogo1.setCancelable(false);
+            dialogo1.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogo1, int id) {
+                    Intent ListSong = new Intent(getApplicationContext(), InicioSesion.class);
+                    startActivity(ListSong);
+
+                }
+
+            });
+            dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogo1, int id) {
+                }
+            });
+            dialogo1.show();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+
+
+        //Toast.makeText(this,"Se seleccionó la segunda opción",Toast.LENGTH_LONG).show();
+
+
+
+    }
+    /*
     //CREACION DEL MENU ACTIONBAR
     @Override
     public boolean onCreateOptionsMenu(android.view.Menu menu) {
@@ -31,6 +80,8 @@ public class Menu extends AppCompatActivity {
         return true;
 
     }
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -60,7 +111,7 @@ public class Menu extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
 
-    }
+    }*/
 
 
 }
