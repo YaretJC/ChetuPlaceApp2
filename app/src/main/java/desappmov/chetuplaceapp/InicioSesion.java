9f64b87ed4e6cc137e45e159ae3e33c1aa46e2ae
yaret <JimenezC_Yaret@outlook.com>
2016-12-10 01:52:04 -0500

    Initial commit

---
+++ app/src/main/java/desappmov/chetuplaceapp/InicioSesion.java
@@ -0,0 +1,14 @@
+package desappmov.chetuplaceapp;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InicioSesion extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_sesion);
+    }
+}
---
+++ app/src/main/java/desappmov/chetuplaceapp/MainActivity.java
@@ -0,0 +1,35 @@
+package desappmov.chetuplaceapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(MainActivity.this,InicioSesion.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
