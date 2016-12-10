a35a86b822683572cdb7a532cc4065d6098e5b5f
yaret <JimenezC_Yaret@outlook.com>
2016-12-10 16:27:04 -0500

    Sesion de usuario

--- app/src/main/java/desappmov/chetuplaceapp/Menu.java
+++ app/src/main/java/desappmov/chetuplaceapp/Menu.java
@@ -1,7 +1,13 @@
 package desappmov.chetuplaceapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class Menu extends AppCompatActivity {
 
@@ -9,5 +15,52 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        //pasa al Siguiente layaut Inicio
+        findViewById(R.id.txtLugares).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Menu.this, Lugares.class));
+            }
+        });
     }
+    //CREACION DEL MENU ACTIONBAR
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.opc) {
+
+           // Toast.makeText(this, "Se ha cerrado la sesión", Toast.LENGTH_LONG).show();
+            AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+            dialogo1.setTitle("Cerrar Sesion");
+            dialogo1.setMessage("¿Desea salir de ChetuPlace?");
+            dialogo1.setCancelable(false);
+            dialogo1.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogo1, int id) {
+                    Intent ListSong = new Intent(getApplicationContext(), InicioSesion.class);
+                    startActivity(ListSong);
+
+                }
+
+            });
+            dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogo1, int id) {
+                }
+            });
+            dialogo1.show();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
 }
