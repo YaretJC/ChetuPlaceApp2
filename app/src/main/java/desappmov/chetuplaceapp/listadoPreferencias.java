a099e3c9f351ce6cdd1ddedcd71cae19524d8000
yaret <JimenezC_Yaret@outlook.com>
2016-12-11 01:37:25 -0500

    agregacion de elementos

--- app/src/main/java/desappmov/chetuplaceapp/listadoPreferencias.java
+++ app/src/main/java/desappmov/chetuplaceapp/listadoPreferencias.java
@@ -1,7 +1,12 @@
 package desappmov.chetuplaceapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class listadoPreferencias extends AppCompatActivity {
 
@@ -10,4 +15,50 @@ public class listadoPreferencias extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado_preferencias);
     }
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        getMenuInflater().inflate(R.menu.menuopciones, menu);
+        return true;
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.opcion1) {
+            Toast.makeText(this, "Se seleccionó la primer opción", Toast.LENGTH_LONG).show();
+        }
+        if (id == R.id.opcion2) {
+            AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+            dialogo1.setTitle("Cerrar Sesion");
+            dialogo1.setMessage("¿Desea salir de ChetuPlace?");
+            dialogo1.setCancelable(false);
+            dialogo1.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogo1, int id) {
+                    Intent ListSong = new Intent(getApplicationContext(), InicioSesion.class);
+                    startActivity(ListSong);
+
+                }
+
+            });
+            dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogo1, int id) {
+                }
+            });
+            dialogo1.show();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+
+
+        //Toast.makeText(this,"Se seleccionó la segunda opción",Toast.LENGTH_LONG).show();
+
+
+
+    }
+
+
 }
+
